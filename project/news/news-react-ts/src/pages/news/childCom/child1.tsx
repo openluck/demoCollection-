106e432b892225aaa43340cf9bc5c218c841443d
openluck <open_luck@163.com>
2021-11-19 17:16:40 +0800

    2
    2

---
+++ project/news/news-react-ts/src/pages/news/childCom/child1.tsx
@@ -0,0 +1,16 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: OpenLcuk
+ * @Date: 2021-11-19 13:10:13
+ * @LastEditors: OpenLcuk
+ * @LastEditTime: 2021-11-19 15:12:52
+ */
+
+import React, { FC } from 'react'
+
+const chid1: FC = () => {
+  return <div>{this.props.username}</div>
+}
+
+export default chid1
--- project/news/news-react-ts/src/pages/news/index.tsx
+++ project/news/news-react-ts/src/pages/news/index.tsx
@@ -1,7 +1,43 @@
-import React, { useEffect, FC } from 'react'
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: OpenLcuk
+ * @Date: 2021-11-19 09:15:34
+ * @LastEditors: OpenLcuk
+ * @LastEditTime: 2021-11-19 13:21:42
+ */
+import React, { useEffect, FC, useState } from 'react'
+import Child from './childCom/child1'
+
+interface newsListProps {
+  count: number;
+}
 
 const News: FC = () => {
-  return (<div>这是新闻页</div>)
+  const [count, setCount] = useState<number>(0)
+  const [robotUrl, setRobotUrl] = useState<any>([])
+  const [username, setUsername] = useState<string>('艾莱客斯')
+
+  // 副作用钩子
+  useEffect(() => {
+    document.title = `${count}次`
+  }, [])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users').then((response) =>
+      response.json().then((data) => {
+        setRobotUrl(data)
+      })
+    )
+  },[])
+
+  return (
+    <div>
+      <p>你点击了{count}次</p>
+      <button onClick={() => setCount(count + 1)}>click</button>
+     <Child username={username}></Child>
+    </div>
+  )
 }
 
 export default News
